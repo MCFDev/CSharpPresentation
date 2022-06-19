820a8a29d7b5b67854f7491ea0a495baf0e9c152
MCF <84239821+MCFDev@users.noreply.github.com>
2022-06-19 00:24:13 +0000

    ga

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿//school presentation
+using static System.Console;
+using Figgle;
+using System.Drawing;
+
+
+namespace helllooo
+{
+    class sex
+    {
+        static void Main(string[] args)
+        {
+                    //setting up figgle and console settngs
+            using var fontStream = System.IO.File.OpenRead("cosmike.flf");
+            var font = FiggleFontParser.Parse(fontStream);
+            Console.Title = "Aula de ICORI CEF 2 - 1ºA";
+            var text = font.Render("Bom Dia");
+            Console.WriteLine(text, ColorTranslator.FromHtml("#8AFFEF"));
+        }
+    }
+}
